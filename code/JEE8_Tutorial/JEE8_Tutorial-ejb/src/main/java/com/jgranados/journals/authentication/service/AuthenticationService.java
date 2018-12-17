2cb2c6bfdca643f2f33e33b3c0d32adf8d79d509
jose <jgranados.iastic@gmail.com>
2018-12-17 11:12:20 -0600

    Cleanning

--- code/JEE8_Tutorial/JEE8_Tutorial-ejb/src/main/java/com/jgranados/journals/authentication/service/AuthenticationService.java
+++ code/JEE8_Tutorial/JEE8_Tutorial-ejb/src/main/java/com/jgranados/journals/authentication/service/AuthenticationService.java
@@ -2,8 +2,6 @@ package com.jgranados.journals.authentication.service;
 
 import com.jgranados.journals.user.domain.User;
 import com.jgranados.journals.user.repository.UserRepository;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 import javax.annotation.Resource;
 import javax.ejb.EJB;
@@ -31,16 +29,6 @@ public class AuthenticationService {
 
     @Inject
     private Pbkdf2PasswordHash pbkdf2PasswordHash;
-
-    public void login() {
-        Map<String, String> map = new HashMap<>();
-        map.put("Pbkdf2PasswordHash.Algorithm", "PBKDF2WithHmacSHA256");
-        map.put("Pbkdf2PasswordHash.SaltSizeBytes", "64");
-        map.put("Pbkdf2PasswordHash.KeySizeBytes", "64");
-        pbkdf2PasswordHash.initialize(map);
-        System.out.println("PASSWOWRD:" + pbkdf2PasswordHash.generate(new char[]{'1', '2', '3', '4', '5'}));
-
-    }
 
     /**
      * Method to get the authenticated user using the securitycontext and the user principal
